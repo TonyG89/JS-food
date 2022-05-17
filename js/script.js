7d269c8cc55c78d46f12465f4edce7b3449cc99f
Fil <a.gryhoryev@gmail.com>
2022-05-17 23:49:54 +0300

    master

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
         tabsContent.forEach(item => {
             item.style.display = "none"
         })
-        
+
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active')
         })
@@ -205,7 +205,7 @@ window.addEventListener('DOMContentLoaded', () => {
         .then(data => {
             data.data.forEach(({
                 img,
-                altimg, 
+                altimg,
                 title,
                 descr,
                 price
@@ -510,11 +510,11 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function deleteNotDigits(str){
+    function deleteNotDigits(str) {
         return +str.replace(/\D/g, '')
     }
 
-   
+
 
     dots.forEach(dot => {
         dot.addEventListener('click', (e) => {
@@ -568,6 +568,114 @@ window.addEventListener('DOMContentLoaded', () => {
         dotsList()
     })
 
+    // CALC
+    const result = document.querySelector('.calculating__result span')
+
+    let sex, height, weight, age, ratio
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex')
+    } else {
+        sex = 'female'
+        localStorage.setItem('sex', 'female')
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio')
+    } else {
+        ratio = '1.375'
+        localStorage.setItem.getItem('ratio', '1.375')
+    }
+
+    function iniLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector)
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass)
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass)
+            }
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass)
+            }
+        })
+    }
+
+    iniLocalSettings('#gender', 'calculating__choose-item_active')
+    iniLocalSettings('.calculating__choose_big', 'calculating__choose-item_active')
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) { // какая-то из переменных будет не заполнена (нет пола или нет роста или нета веса)
+            result.textContent = '_____'
+            return
+        }
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio) // округления в целое
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio)
+
+        }
+    }
+
+
+
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(`${selector}`) // берем с родительского эллемента.
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio')
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio')) // занесение данный в локалстораж
+                } else {
+                    sex = e.target.getAttribute('id')
+                    localStorage.setItem('sex', e.target.getAttribute('id')) // занесение данный в локалстораж
+                }
+
+                elements.forEach(elem => { // каждый активный элемент ... 
+                    elem.classList.remove(activeClass) // убираем подсветку кнопки при нажатии
+                })
+                e.target.classList.add(activeClass) // добавляем подсветку при нажатии 
+
+                calcTotal()
+            })
+        })
+
+    }
+
+    getStaticInformation('#gender div', 'calculating__choose-item_active')
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active')
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector)
+
+        input.addEventListener('input', () => {
+
+            if (input.value.match(/\D/g)) { // если там не число
+                input.style.border = '1px solid red'
+                res
+            } else {
+                input.style.border = 'none'
+                switch (input.getAttribute('id')) { // если в указаной кейсом id будет значение
+                    case 'height':
+                        height = +input.value
+                        break
+                    case 'weight':
+                        weight = +input.value
+                        break
+                    case 'age':
+                        age = +input.value
+                        break
+                }
+            }
+
+
+            calcTotal()
+        })
+
+    }
+    getDynamicInformation('#height')
+    getDynamicInformation('#weight')
+    getDynamicInformation('#age')
 
 
 })
